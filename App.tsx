ca1c2ce67eb13a8e87a42f16b5faaff4e3268dc8
seojin kim <001106ksj@gmail.com>
2022-01-28 03:12:59 +0900

    feat: 이미지 blob 업로드 기능 테스트
    https://blog.waldo.io/add-an-image-picker-react-native-app/

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, TextInput, View, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [image, setImage] = useState<string | null>(null);
+  const uploadImage = async () => {
+    const _image = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+    });
+    if (!_image.cancelled) setImage(_image.uri);
+  };
+
+  const checkCameraPermission = async () => {
+    const { status } = await ImagePicker.getMediaLibraryPermissionsAsync();
+    //if (status !== 'granted') alert('카메라 권한을 허용해주세요');
+  };
+
+  useEffect(() => {
+    checkCameraPermission();
+  }, []);
+
+  useEffect(() => {
+    console.log(image);
+  }, [image]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+      <TextInput style={styles.input} placeholder='글자 입력' />
+      <Button title='사진 입력' onPress={uploadImage} />
     </View>
   );
 }
@@ -16,5 +40,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+  input: {
+    width: '90%',
+    padding: 12,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 16,
   },
 });

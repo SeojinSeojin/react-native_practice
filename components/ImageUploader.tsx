d52727830f380a364909195b5a62244f88888dce
seojin kim <001106ksj@gmail.com>
2022-01-31 02:04:15 +0900

    feat: 이미지 서버 업로드 기능 구현

--- components/ImageUploader.tsx
+++ components/ImageUploader.tsx
@@ -1,14 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Image, Button } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { postFetcher } from '../utils/postFetcher';
+import { dataURItoBLOB } from '../utils/blobExtractor';
 
 export default function ImageUploader() {
   const [image, setImage] = useState<string | null>(null);
-  const uploadImage = async () => {
+  const getImage = async () => {
     const _image = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
     });
     if (!_image.cancelled) setImage(_image.uri);
+  };
+
+  const uploadImage = async () => {
+    if (!image) return;
+    const formData = new FormData();
+    formData.append('file', dataURItoBLOB(image));
+    const response = await postFetcher(
+      'http://localhost:4444/upload',
+      formData,
+      'multipart'
+    );
+    if (!response.ok) alert('사진 전송 실패!');
+    else alert('사진 전송 성공!');
   };
 
   const checkCameraPermission = async () => {
@@ -23,7 +38,8 @@ export default function ImageUploader() {
   return (
     <View>
       {image && <Image source={{ uri: image }} style={styles.image} />}
-      <Button title='사진 입력' onPress={uploadImage} />
+      <Button title='사진 입력' onPress={getImage} />
+      <Button title='사진 전송' onPress={uploadImage} />
     </View>
   );
 }

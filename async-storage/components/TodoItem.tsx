6dfb67ebd71effe7edc800ea54f7c4efcfabef3e
seojin kim <001106ksj@gmail.com>
2022-02-09 23:12:59 +0900

    feat: 투두리스트 상태에 따라 스타일 주기

--- async-storage/components/TodoItem.tsx
+++ async-storage/components/TodoItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-native';
+import { Button, StyleSheet, View, Text } from 'react-native';
 
 function TodoItem(props: {
   todo: Todo;
@@ -9,17 +9,39 @@ function TodoItem(props: {
 }) {
   const { todo, onEdit, onComplete, onDelete } = props;
   return (
-    <div>
-      <div>{todo.content}</div>
+    <View
+      style={[
+        styles.wrapper,
+        todo.isCompleted
+          ? styles.completed
+          : todo.isDeleted
+          ? styles.deleted
+          : null,
+      ]}
+    >
+      <Text>{todo.content}</Text>
       {!todo.isCompleted && !todo.isDeleted && (
-        <>
+        <View style={styles.buttonWrapper}>
           <Button title='수정' onPress={() => onEdit(todo.id)} />
           <Button title='완료' onPress={() => onComplete(todo.id)} />
           <Button title='삭제' onPress={() => onDelete(todo.id)} />
-        </>
+        </View>
       )}
-    </div>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    width: '100%',
+    height: 36,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  buttonWrapper: { flexDirection: 'row' },
+  completed: { flexDirection: 'row-reverse', backgroundColor: 'yellow' },
+  deleted: { flexDirection: 'row-reverse', backgroundColor: 'red' },
+});
 
 export default TodoItem;
